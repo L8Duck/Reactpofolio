c46b9a4ed14cac93559c7f38d0bee5357d3a537e
L8duck <ndkhoa1000@gmail.com>
2024-09-25 10:28:12 +0700

    switch BrowserRoute to HashRoute for deploying to github page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -78,8 +78,8 @@ function Home() {
           {tagline}
         </h2>
       </div>
-      <div className="mt-5 md:mt-0">
-        <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Pavan MG" />
+      <div className="mt-3 md:mt-0">
+        <img ref={myimageref} className=" md:ml-2" src={img} alt="Nguyen Dang Khoa" />
       </div>
     </main>
   );
